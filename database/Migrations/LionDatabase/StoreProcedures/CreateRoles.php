05060b70e9ad7d8ffc79584fef8e2626ba9c63b7
Sleon4 <sergioleon4004@hotmail.com>
2024-02-19 12:08:08 -0500

    build: trait has been removed in migrations

--- database/Migrations/LionDatabase/StoreProcedures/CreateRoles.php
+++ database/Migrations/LionDatabase/StoreProcedures/CreateRoles.php
@@ -3,14 +3,11 @@
 declare(strict_types=1);
 
 use Lion\Bundle\Interface\MigrationUpInterface;
-use Lion\Bundle\Traits\Migration;
 use Lion\Database\Drivers\MySQL;
 use Lion\Database\Drivers\Schema\MySQL as Schema;
 
 return new class implements MigrationUpInterface
 {
-	use Migration;
-
 	/**
 	 * {@inheritdoc}
 	 * */
