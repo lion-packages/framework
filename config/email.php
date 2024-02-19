41282beaf55964b0b09790aa0c4e6678c5f594e5
Sleon4 <sergioleon4004@hotmail.com>
2024-02-19 12:12:39 -0500

    build: composer dependencies have been updated

--- config/email.php
+++ config/email.php
@@ -20,15 +20,5 @@ Mailer::initialize([
         'port' => (int) env->MAIL_PORT,
         'encryption' => env->MAIL_ENCRYPTION,
         'debug' => (bool) env->MAIL_DEBUG
-    ],
-    env->MAIL_NAME_SUPP => [
-        'name' => env->MAIL_NAME_SUPP,
-        'type' => env->MAIL_TYPE_SUPP,
-        'host' => env->MAIL_HOST_SUPP,
-        'username' => env->MAIL_USER_NAME_SUPP,
-        'password' => env->MAIL_PASSWORD_SUPP,
-        'port' => (int) env->MAIL_PORT_SUPP,
-        'encryption' => env->MAIL_ENCRYPTION_SUPP,
-        'debug' => (bool) env->MAIL_DEBUG_SUPP
     ]
 ], env->MAIL_NAME);
