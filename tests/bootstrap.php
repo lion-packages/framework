6a627bd1e0890b498eca7de74c5a1d6490894924
Sleon4 <sergioleon4004@hotmail.com>
2024-02-24 16:11:07 -0500

    ci: bootstrap file has been added for testing configuration

---
+++ tests/bootstrap.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+define('LION_START', microtime(true));
+
+/**
+ * -----------------------------------------------------------------------------
+ * Register The Auto Loader
+ * -----------------------------------------------------------------------------
+ * Composer provides a convenient, automatically generated class loader for
+ * this application
+ * -----------------------------------------------------------------------------
+ **/
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+use Dotenv\Dotenv;
+
+/**
+ * -----------------------------------------------------------------------------
+ * Register environment variable loader automatically
+ * -----------------------------------------------------------------------------
+ * .dotenv provides an easy way to access environment variables with $_ENV
+ * -----------------------------------------------------------------------------
+ **/
+Dotenv::createImmutable(__DIR__ . '/../')->load();
+
+/**
+ * -----------------------------------------------------------------------------
+ * Database initialization
+ * -----------------------------------------------------------------------------
+ * */
+include_once(__DIR__ . '/../config/database.php');
+
+/**
+ * -----------------------------------------------------------------------------
+ * Email initialization
+ * -----------------------------------------------------------------------------
+ * */
+include_once(__DIR__ . '/../config/email.php');
+
+/**
+ * -----------------------------------------------------------------------------
+ * Local zone configuration
+ * -----------------------------------------------------------------------------
+ */
+date_default_timezone_set($_ENV['SERVER_DATE_TIMEZONE']);
