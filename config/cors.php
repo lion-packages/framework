89d96dd16b5e7d0c7786d07eaeb07e577896354c
Sleon4 <sergioleon4004@hotmail.com>
2024-03-23 18:37:15 -0500

    refactor: headers have been rearranged

--- config/cors.php
+++ config/cors.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use Lion\Request\Request;
+use Lion\Route\Route;
+
 /**
  * -----------------------------------------------------------------------------
  * Cross-Origin Resource Sharing (CORS) Configuration
@@ -12,11 +15,20 @@ declare(strict_types=1);
  * -----------------------------------------------------------------------------
  **/
 
-return [
-    'Access-Control-Allow-Origin' => '*',
-    'Content-Type' => 'application/json; charset=UTF-8',
-    'Access-Control-Max-Age' => '0',
-    'Allow' => 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-];
+Request::header('Access-Control-Allow-Origin', '*');
+
+Request::header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+Request::header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] === Route::OPTIONS) {
+    http_response_code(200);
+
+    exit;
+}
+
+Request::header('Content-Type', 'application/json; charset=UTF-8');
+
+Request::header('Access-Control-Max-Age', '0');
+
+Request::header('Allow', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
