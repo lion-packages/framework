130da32f4f321b041adc7d242bf75aa801c0e089
Sleon4 <sergioleon4004@hotmail.com>
2024-05-05 16:54:17 -0500

    test: added test for PasswordManagerService

--- tests/App/Http/Services/LionDatabase/MySQL/PasswordManagerServiceTest.php
+++ tests/App/Http/Services/LionDatabase/MySQL/PasswordManagerServiceTest.php
@@ -7,18 +7,24 @@ namespace Tests\App\Http\Services\LionDatabase\MySQL;
 use App\Exceptions\PasswordException;
 use App\Http\Services\LionDatabase\MySQL\PasswordManagerService;
 use Database\Factory\LionDatabase\MySQL\UsersFactory;
+use Lion\Dependency\Injection\Container;
 use Lion\Request\Request;
 use Lion\Security\Validation;
 use Lion\Test\Test;
+use Tests\Providers\SetUpMigrationsAndQueuesProviderTrait;
 
 class PasswordManagerServiceTest extends Test
 {
+    use SetUpMigrationsAndQueuesProviderTrait;
+
     private PasswordManagerService $passwordManagerService;
     private Validation $validation;
 
     protected function setUp(): void
     {
-        $this->passwordManagerService = new PasswordManagerService();
+        $this->runMigrationsAndQueues();
+
+        $this->passwordManagerService = (new Container())->injectDependencies(new PasswordManagerService());
 
         $this->validation = new Validation();
     }
@@ -47,5 +53,10 @@ class PasswordManagerServiceTest extends Test
         $passwordEntered = $this->validation->sha256(UsersFactory::USERS_PASSWORD . '-X');
 
         $this->passwordManagerService->comparePasswords($usersPassword, $passwordEntered);
+    }
+
+    public function testUpdatePassword(): void
+    {
+        $this->expectNotToPerformAssertions();
     }
 }
