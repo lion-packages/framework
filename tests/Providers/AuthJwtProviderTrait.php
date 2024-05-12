b64054e987b38384ce5b0e001a2963f9a05de491
Sleon4 <sergioleon4004@hotmail.com>
2024-05-12 13:12:59 -0500

    test: tests for JWTMiddleware have been added

--- tests/Providers/AuthJwtProviderTrait.php
+++ tests/Providers/AuthJwtProviderTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Providers;
 
+use Lion\Command\Kernel;
 use Lion\Security\JWT;
 use Lion\Security\RSA;
 
@@ -12,12 +13,36 @@ trait AuthJwtProviderTrait
     const AVAILABLE_USERS = 2;
     const REMAINING_USERS = 1;
 
+    private function generateKeys(string $path): void
+    {
+        (new Kernel())
+            ->execute("php lion new:rsa --path keys/{$path}/", false);
+    }
+
     private function getAuthorization(array $data = []): string
     {
         $token = (new JWT)
             ->config([
                 'privateKey' => (new RSA())
-                    ->setUrlPath(storage_path(env('RSA_URL_PATH'), false))
+                    ->setUrlPath(env('RSA_URL_PATH'))
+                    ->init()
+                    ->getPrivateKey()
+            ])
+            ->encode([
+                'session' => true,
+                ...$data
+            ], (int) env('JWT_EXP', 3600))
+            ->get();
+
+        return "Bearer {$token}";
+    }
+
+    public function getCustomAuthorization(string $path, array $data = []): string
+    {
+        $token = (new JWT)
+            ->config([
+                'privateKey' => (new RSA())
+                    ->setUrlPath(env('RSA_URL_PATH') . $path)
                     ->init()
                     ->getPrivateKey()
             ])
