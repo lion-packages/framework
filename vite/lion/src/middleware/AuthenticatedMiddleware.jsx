61a68d5794cf2d492c6299ba25b632d604b3d22e
Sleon4 <sergioleon4004@hotmail.com>
2024-05-27 16:36:16 -0500

    chore: added validation for user authentication middleware

--- vite/lion/src/middleware/AuthenticatedMiddleware.jsx
+++ vite/lion/src/middleware/AuthenticatedMiddleware.jsx
@@ -1,8 +1,14 @@
+/* eslint-disable react/prop-types */
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
+import LoadingScreen from "../pages/components/LoadingScreen";
 
 export default function AuthenticatedMiddleware({ children }) {
-  const { jwt } = useAuth();
+  const { jwt, loadingJWT } = useAuth();
+
+  if (loadingJWT) {
+    return <LoadingScreen />;
+  }
 
   if ([null, undefined].includes(jwt)) {
     return <Navigate to="/auth/login" replace />;
