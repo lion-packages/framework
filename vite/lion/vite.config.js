c325d2f7ec96da49b992319ee8952db774ba4d20
Sleon4 <sergioleon4004@hotmail.com>
2024-05-30 10:30:17 -0500

    test: added test for AlertResponse component

--- vite/lion/vite.config.js
+++ vite/lion/vite.config.js
@@ -12,4 +12,10 @@ export default defineConfig({
       usePolling: true,
     },
   },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    css: true,
+  },
 });
