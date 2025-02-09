51e02f53f0bb00e4d1918b992b922a317d2e3c67
Sleon4 <sergioleon4004@hotmail.com>
2025-02-09 16:14:09 -0500

    chore: testing configuration is updated

--- routes/middleware.php
+++ routes/middleware.php
@@ -2,9 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Enums\RolesEnum;
-use App\Http\Middleware\JWTMiddleware;
-use App\Http\Middleware\RolesMiddleware;
 use Lion\Bundle\Helpers\Http\Routes;
 use Lion\Bundle\Middleware\HttpsMiddleware;
 use Lion\Bundle\Middleware\RouteMiddleware;
@@ -30,27 +27,5 @@ Routes::setMiddleware([
      * [Filters that request via HTTPS]
      */
     new Middleware('https', HttpsMiddleware::class, 'https'),
-
-    /**
-     * [Filters to validate different states with JWT]
-     */
-
-    new Middleware('jwt-existence', JWTMiddleware::class, 'existence'),
-
-    new Middleware('jwt-authorize', JWTMiddleware::class, 'authorize'),
-
-    new Middleware('jwt-not-authorize', JWTMiddleware::class, 'notAuthorize'),
-
-    new Middleware('jwt-without-signature', JWTMiddleware::class, 'authorizeWithoutSignature'),
-
-    /**
-     * Filters to validate user roles
-     */
-
-    new Middleware('admin-access', RolesMiddleware::class, 'access', [
-        'roles' => [
-            RolesEnum::ADMINISTRATOR->value,
-        ],
-    ]),
 
 ]);
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\LionDatabase\MySQL\AuthenticatorController;
-use App\Http\Controllers\LionDatabase\MySQL\LoginController;
-use App\Http\Controllers\LionDatabase\MySQL\PasswordManagerController;
-use App\Http\Controllers\LionDatabase\MySQL\ProfileController;
-use App\Http\Controllers\LionDatabase\MySQL\RegistrationController;
-use App\Http\Controllers\LionDatabase\MySQL\UsersController;
 use Lion\Route\Route;
 
 /**
@@ -18,46 +12,4 @@ use Lion\Route\Route;
  * -----------------------------------------------------------------------------
  */
 
-Route::middleware(['https'], function (): void {
-    Route::get('/', fn (): stdClass => info('[index]'));
-
-    Route::prefix('api', function (): void {
-        Route::prefix('auth', function (): void {
-            Route::post('login', [LoginController::class, 'auth']);
-            Route::post('2fa', [LoginController::class, 'auth2FA']);
-            Route::post('register', [RegistrationController::class, 'register']);
-            Route::post('verify', [RegistrationController::class, 'verifyAccount']);
-            Route::post('refresh', [LoginController::class, 'refresh'], ['jwt-existence']);
-
-            Route::prefix('recovery', function (): void {
-                Route::post('password', [PasswordManagerController::class, 'recoveryPassword']);
-                Route::post('verify-code', [PasswordManagerController::class, 'updateLostPassword']);
-            });
-        });
-
-        Route::middleware(['jwt-authorize'], function (): void {
-            Route::prefix('profile', function (): void {
-                Route::get('/', [ProfileController::class, 'readProfile']);
-                Route::put('/', [ProfileController::class, 'updateProfile']);
-                Route::post('password', [PasswordManagerController::class, 'updatePassword']);
-
-                Route::controller(AuthenticatorController::class, function (): void {
-                    Route::post('2fa/verify', 'passwordVerify');
-                    Route::get('2fa/qr', 'qr');
-                    Route::post('2fa/enable', 'enable2FA');
-                    Route::post('2fa/disable', 'disable2FA');
-                });
-            });
-
-            Route::middleware(['admin-access'], function (): void {
-                Route::controller(UsersController::class, function (): void {
-                    Route::post('users', 'createUsers');
-                    Route::get('users', 'readUsers');
-                    Route::get('users/{idusers:i}', 'readUsersById');
-                    Route::put('users/{idusers:i}', 'updateUsers');
-                    Route::delete('users/{idusers:i}', 'deleteUsers');
-                });
-            });
-        });
-    });
-});
+Route::get('/', fn (): stdClass => info('[index]'));
